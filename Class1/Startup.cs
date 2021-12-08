6e8cc9d62f57e68975694fcda6ca67115b227c75
Moshkovsky_96 <Moshkovsky_96@ukr.net>
2021-12-08 20:58:04 +0200

    q1

--- Class1/Startup.cs
+++ Class1/Startup.cs
@@ -13,35 +13,35 @@ namespace Class1
             User user1 = new User() { Email = "programist", Password = "qwerty12345", Id = "529" };
             User user2 = new User() { Email = "programmer", Password = "programmer", Id = "530" };
             User user3 = new User() { Email = "vlad96", Password = "vlad96", Id = "531" };
-           
 
             Console.WriteLine("Введите Email");
             string email = Console.ReadLine();
             Console.WriteLine("Введите пароль");
             string password = Console.ReadLine();
 
-            User[] users = new User [] { user1, user2, user3 };
-            bool result = false ;
+            User[] users = new User[] { user1, user2, user3 };
+
+            CheckUsers(email, password,users);
+        }
+        public void CheckUsers(string email, string password, User[] users)
+        {
+            bool result = false;
 
             foreach (var item in users)
             {
-                 result = item.Check(email, password);
-                
-                    if (result == true)
-                    {
-                        Console.WriteLine("Авторизация успешна");
-                        break;
-                    }
-                              
+                result = item.Check(email, password);
 
+                if (result == true)
+                {
+                    Console.WriteLine("Авторизация успешна");
+                    break;
+                }
             }
             if (!result)
             {
                 Console.WriteLine("Авторизация не успешна");
 
             }
-
-
 
         }
 
